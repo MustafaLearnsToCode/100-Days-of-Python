68bc4afbe2f29b69054df70f2a325456d5b7bf19
MustafaLearnsToCode <badshah.mustafa77@gmail.com>
2025-07-27 19:01:53 -0400

    Add Day 23 - Turtle Crossy Road Game

---
+++ Day 23 - Turtle Crossy Road [capstone]/main.py
@@ -0,0 +1,40 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Turtle Crossy Road")
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(fun=player.move, key="space")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    #detect collision with cars
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    #detect player crossed
+    if player.at_end():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.update_level()
+
+
+screen.exitonclick()
